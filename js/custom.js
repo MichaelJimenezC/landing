25b112fe4238f33c7ff1a1b00ca0beba2a185e42
MichaelJimenezC <micjejim@espol.edu.ec>
2023-11-04 16:19:35 -0500

    Modal checkout

--- js/custom.js
+++ js/custom.js
@@ -31,11 +31,9 @@
   })(window.jQuery);
 
   document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el valor del parámetro "plan" de la URL
     const urlParams = new URLSearchParams(window.location.search);
     const selectedPlan = urlParams.get('plan');
 
-    // Comprobar qué plan se ha seleccionado y marcarlo como seleccionado
     if (selectedPlan === 'fitmi') {
         document.getElementById('flexRadioDefault1').checked = true;
     } else if (selectedPlan === 'hercules') {
@@ -44,5 +42,94 @@
         document.getElementById('flexRadioDefault3').checked = true;
     }
 });
+$(document).ready(function() {
+  $("a[href='#nav-ContactMap-tab']").click(function() {
+      $('#nav-ContactMap-tab').tab('show');
+
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const radioButtons = document.querySelectorAll('input[name="TicketForm"]');
+  const subtotalElement = document.getElementById("subtotal");
+  const totalElement = document.getElementById("total");
+  function actualizarPrecios() {
+      let precio = 0;
+      for (const radioButton of radioButtons) {
+          if (radioButton.checked) {
+              if (radioButton.id === "flexRadioDefault1") {
+                  precio = 120;
+              } else if (radioButton.id === "flexRadioDefault2") {
+                  precio = 240;
+              } else if (radioButton.id === "flexRadioDefault3") {
+                  precio = 480;
+              }
+              break;
+          }
+      }
+      subtotalElement.textContent = `$${precio}`;
+      totalElement.textContent = `$${precio}`;
+  }
+  actualizarPrecios();
+  for (const radioButton of radioButtons) {
+      radioButton.addEventListener("change", actualizarPrecios);
+  }
+});
 
 
+// Espera a que el documento esté listo
+$(document).ready(function () {
+  // Maneja el clic en el botón "Comprar"
+  $('#checkout-Comprar').click(function () {
+    // Validar si todos los campos están llenos
+    if (validarCampos()) {
+      // Cierra el modal de pago
+      $('#paymentModal').modal('hide');
+
+      // Abre el modal de agradecimiento
+      $('#agradecimientoModal').modal('show');
+    } else {
+      // Muestra una alerta o mensaje de error
+      alert('Por favor, complete todos los campos antes de continuar.');
+    }
+  });
+
+  // Función para validar que todos los campos estén llenos
+  function validarCampos() {
+    // Comprueba si todos los campos obligatorios están llenos
+    var cardNumber = $('#cardNumber').val();
+    var nameOnCard = $('#nameOnCard').val();
+    var expiryDate = $('#expiryDate').val();
+    var cvvCode = $('#cvvCode').val();
+   
+
+    return cardNumber && nameOnCard && expiryDate && cvvCode  ;
+  }
+});
+$(document).ready(function () {
+  // Maneja el clic en el botón "Comprar"
+  $('#btn_comprar').click(function (e) {
+    // Evita el envío del formulario por defecto
+    e.preventDefault();
+
+    // Validar si todos los campos obligatorios, excepto el textarea, están llenos
+    if (validarCampos()) {
+      // Cierra el modal de agradecimiento si está abierto
+      $('#agradecimientoModal').modal('hide');
+
+      // Abre el modal de pago
+      $('#paymentModal').modal('show');
+    } else {
+      // Muestra una alerta o mensaje de error
+      alert('Por favor, complete todos los campos obligatorios antes de continuar.');
+    }
+  });
+
+  // Función para validar que todos los campos obligatorios, excepto el textarea, estén llenos
+  function validarCampos() {
+    var nombre = $('#ticket-form-name').val();
+    var email = $('#ticket-form-email').val();
+    var telefono = $('#ticket-form-phone').val();
+
+    return nombre && email && telefono ;
+  }
+});
